0f5895b8ccc23a48125c598c290e134d0a5ef063
Ratna Purwati <ratna.purwati@ymail.com>
2022-02-23 14:51:16 +0800

    Rails new with frontend and form gems

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+p "start"
+5.times do
+  Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.city, phone_number: Faker::PhoneNumber.cell_phone, category: Restaurant::CATEGORY.sample)
+end
+
+p "done"
